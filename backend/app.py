9e765278d9dd5142ebc2b98cb96f78e205c6a058
ARNAB GHOSH <arnabghosh31031998@gmail.com>
2025-09-27 23:54:27 +0530

    Fixed origins of backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "https://stirring-genie-9833a8.netlify.app"
-]
+if os.path.exists(".env.development") and not os.getenv("PRODUCTION"):
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://localhost:5173",
+    ]
+else:
+    origins = [
+        "https://stirring-genie-9833a8.netlify.app"
+    ]
+
 
 app: FastAPI = FastAPI()
 
